eab726026109b13c63dc7bdeeb6a0f5847c0a907
Chaoren Lin <chaoren@binghamton.edu>
2014-02-15 06:19:53 -0500

    commit

--- src/test.c
+++ src/test.c
@@ -1,20 +1,24 @@
 #include <pebble.h>
 
 static Window *window = NULL;
-static BitmapLayer *bitmap = NULL;
+static Layer *root = NULL;
+
+void update(Layer *layer, GContext *ctx) {
+	graphics_draw_circle(ctx, GPoint(0, 0), 20);
+}
 
 void init(void) {
 	window = window_create();
 	window_set_background_color(window, GColorBlack);
 	window_stack_push(window, true);
 
-	bitmap = bitmap_layer_create(GRect(0, 0, 20, 20));
-	bitmap_layer_set_background_color(bitmap, GColorWhite);
+	root = window_get_root_layer(window);
+	layer_set_update_proc(root, update);
+	layer_mark_dirty(root);
 }
 
 void deinit(void) {
 	window_destroy(window);
-	bitmap_layer_destroy(bitmap);
 }
 
 int main(void) {
