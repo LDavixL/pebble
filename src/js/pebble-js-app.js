a3fd2d148618bd095dc574e57055a7152b2e70c4
Chaoren Lin <clin18@users.noreply.github.com>
2014-02-15 15:22:31 -0800

    works except body

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,4 +1,4 @@
-function fetchWiki() {
+function fetchWiki(key) {
 	var response;
 	var req = new XMLHttpRequest();
 	req.open('GET', "http://en.wikipedia.org/wiki/Main_Page", true);
@@ -6,12 +6,23 @@ function fetchWiki() {
 		if (req.readyState == 4) {
 			if(req.status == 200) {
 				response = req.responseText;
-				var article = /<p.*?p>/.exec(response)[0];
-				article = article.replace(/<(?:.|\n)*?>/gm, '');
-				article = article.replace('&#160;', ' ');
-				article = article.replace(' (Full&#160;article...)', '');
-				Pebble.sendAppMessage({
-					"article": article });
+				var section = /<p.*?p>/.exec(response)[0];
+				section = section.replace(/<(?:.|\n)*?>/gm, '');
+				section = section.replace('&#160;', ' ');
+				section = section.replace(' (Full&#160;article...)', '');
+				switch(key) {
+					case 'article': Pebble.sendAppMessage({
+								"article": section });
+						break;
+					case 'news': Pebble.sendAppMessage({
+								"news": section });
+						break;
+					case 'trivia': Pebble.sendAppMessage({
+								"trivia": section });
+						break;
+					case 'history': Pebble.sendAppMessage({
+								"history": section });
+				}
 			} else {
 				console.log("Request returned error code " + req.status.toString());
 			}
@@ -20,4 +31,7 @@ function fetchWiki() {
 	req.send(null);
 }
 
-Pebble.addEventListener("ready", function(e) { fetchWiki(); });
+Pebble.addEventListener("appmessage", function(e) {
+	for(var key in e.payload) break;
+	fetchWiki(key);
+});
--- src/wiki.c
+++ src/wiki.c
@@ -6,17 +6,18 @@ static TextLayer *title_layer;
 static TextLayer *section_layer;
 static ScrollLayer *scroll_layer;
 
+void select(int index, void *context);
+
 SimpleMenuItem items[] = {
-	{ .icon = NULL, .subtitle = NULL, .title = "Article", },
-	{ .icon = NULL, .subtitle = NULL, .title = "News" },
-	{ .icon = NULL, .subtitle = NULL, .title = "Trivia" },
-	{ .icon = NULL, .subtitle = NULL, .title = "History" },
+	{ .callback = select, .title = "Article", },
+	{ .callback = select, .title = "News" },
+	{ .callback = select, .title = "Trivia" },
+	{ .callback = select, .title = "History" },
 };
 
 const SimpleMenuSection sections = {
 	.items = items,
 	.num_items = 4,
-	.title = NULL,
 };
 
 int8_t wiki = 0x0;
@@ -33,7 +34,6 @@ enum {
 	WIKI_KEY_NEWS = 0x1,
 	WIKI_KEY_KNOW = 0x2,
 	WIKI_KEY_DAY = 0x3,
-	//WIKI_KEY_PIC = 0x4,
 };
 
 static void in_received_handler(DictionaryIterator *iter, void *context) {
@@ -41,12 +41,22 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
 	text_layer_set_text(title_layer, titles[section_tuple->key]);
 	text_layer_set_text(section_layer, section_tuple->value->cstring);
 	int16_t h = text_layer_get_content_size(section_layer).h;
-	text_layer_set_size(section_layer, GSize(144, h));
 	scroll_layer_set_content_size(scroll_layer, GSize(144, 20 + h));
 }
 
 static void in_dropped_handler(AppMessageResult reason, void *context) {
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Dropped!");
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Dropped!");
+}
+
+void select(int index, void *context) {
+	Tuplet tuple = TupletInteger(index, 0);
+	DictionaryIterator *iter;
+	app_message_outbox_begin(&iter);
+	if (iter == NULL) return;
+	dict_write_tuplet(iter, &tuple);
+	dict_write_end(iter);
+	app_message_outbox_send();
+	window_stack_push(subwindow, true);
 }
 
 static void app_message_init(void) {
@@ -56,8 +66,11 @@ static void app_message_init(void) {
 				   APP_MESSAGE_OUTBOX_SIZE_MINIMUM);
 }
 
-static void subwindow_load(Window *window) {
-	Layer *window_layer = window_get_root_layer(subwindow);
+static void window_load(Window *window) {
+	menu_layer = simple_menu_layer_create(GRect(0, 0, 144, 152),
+										  window, &sections, 1, NULL);
+	layer_add_child(window_get_root_layer(window),
+					simple_menu_layer_get_layer(menu_layer));
 	
 	title_layer = text_layer_create(GRect(0, 0, 144, 20));
 	text_layer_set_text_alignment(title_layer, GTextAlignmentCenter);
@@ -65,6 +78,7 @@ static void subwindow_load(Window *window) {
 	section_layer = text_layer_create(GRect(0, 20, 144,
 		app_message_inbox_size_maximum()));
 	text_layer_set_text_alignment(section_layer, GTextAlignmentLeft);
+	text_layer_set_overflow_mode(section_layer, GTextOverflowModeWordWrap);
 	
 	scroll_layer = scroll_layer_create(GRect(0, 0, 144, 152));
 	scroll_layer_set_content_size(scroll_layer, GSize(144, 152));
@@ -72,50 +86,33 @@ static void subwindow_load(Window *window) {
 	
 	scroll_layer_add_child(scroll_layer, text_layer_get_layer(title_layer));
 	scroll_layer_add_child(scroll_layer, text_layer_get_layer(section_layer));
-	layer_add_child(window_layer, scroll_layer_get_layer(scroll_layer));
-}
-
-static void subwindow_unload(Window *window) {
-	text_layer_destroy(title_layer);
-	text_layer_destroy(section_layer);
-	scroll_layer_destroy(scroll_layer);
-}
-static void window_load(Window *window) {
-
-	menu_layer = simple_menu_layer_create(GRect(0, 0, 144, 152),
-										  window, &sections, 4, NULL);
+	layer_add_child(window_get_root_layer(subwindow),
+					scroll_layer_get_layer(scroll_layer));
 }
 
 static void window_unload(Window *window) {
+	text_layer_destroy(title_layer);
+	text_layer_destroy(section_layer);
+	scroll_layer_destroy(scroll_layer);
 	simple_menu_layer_destroy(menu_layer);
 }
 
-static void subinit(void) {
-	subwindow = window_create();
-	window_set_window_handlers(subwindow, (WindowHandlers) {
-		.load = subwindow_load,
-		.unload = subwindow_unload,
-	});
-	window_stack_push(subwindow, true);
-}
-
-static void subdeinit(void) {
-	window_destroy(subwindow);
-}
-
 static void init(void) {
-	window = window_create();
 	app_message_init();
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Init!");
+	
+	window = window_create();
 	window_set_window_handlers(window, (WindowHandlers) {
 		.load = window_load,
 		.unload = window_unload,
 	});
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Window Handlers!!");
+	
+	subwindow = window_create();
+	
 	window_stack_push(window, true);
 }
 
 static void deinit(void) {
+	window_destroy(subwindow);
 	window_destroy(window);
 }
 
