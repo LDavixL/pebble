05163a7ac8ae63d2e22f7a319c70c124f4fd2ca4
Chaoren Lin <chaoren@binghamton.edu>
2014-02-15 06:06:36 -0500

    commit

--- src/test.c
+++ src/test.c
@@ -1,14 +1,14 @@
 #include <pebble.h>
 
+static Window *window = NULL;
 
 void init(void) {
-	Window *window = window_create();
+	window = window_create();
 	window_set_background_color(window, GColorBlack);
 	window_stack_push(window, true);
 }
 
 void deinit(void) {
-	Window *window = window_stack_pop(true);
 	window_destroy(window);
 }
 
