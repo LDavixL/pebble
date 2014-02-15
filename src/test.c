dd4b953c3ebc864ce37da77cec8ae59b6cacd10f
Chaoren Lin <chaoren@binghamton.edu>
2014-02-15 06:10:23 -0500

    commit

--- src/test.c
+++ src/test.c
@@ -1,15 +1,19 @@
 #include <pebble.h>
 
 static Window *window = NULL;
+static BitmapLayer *bitmap = NULL;
 
 void init(void) {
 	window = window_create();
 	window_set_background_color(window, GColorBlack);
 	window_stack_push(window, true);
+
+	bitmap = bitmap_layer_create(GRect(0, 0, 20, 20));
 }
 
 void deinit(void) {
 	window_destroy(window);
+	bitmap_layer_destroy(bitmap);
 }
 
 int main(void) {
